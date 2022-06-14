b612760c9108d475e4dfaf96f5e2a37cbd09c57d
DaCatw_UMVR_PC <memo123449@gmail.com>
2022-06-14 20:27:03 +0800

    Create solution

---
+++ ChatClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string hostIP = "127.0.0.1";
+            const int port = 4099;
+
+            Console.WriteLine("============");
+            var client = new TcpClient();
+
+            try
+            {
+                Console.WriteLine($"Connect to server {hostIP}:{port}");
+                client.Connect(hostIP, port);
+
+                if (!client.Connected)
+                {
+                    Console.WriteLine("Connect faile");
+                    return;
+                }
+
+                Console.WriteLine("Connect success");
+            }
+            catch(ArgumentNullException e)
+            {
+                Console.WriteLine($"ArgumentNullException: {e}");
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e}");
+            }
+            finally
+            {
+                client.Close();
+                Console.WriteLine("Disconnected");
+            }
+        }
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 4099;
+
+            Console.WriteLine("============");
+            var server = new TcpListener(IPAddress.Any, port);
+
+            try
+            {
+                Console.WriteLine("Server Start");
+                server.Start();
+
+                Console.WriteLine("Waiting for client...");
+                var client = server.AcceptTcpClient();
+
+                var address = client.Client.RemoteEndPoint.ToString();
+                Console.WriteLine($"Client has connect {address}");
+
+                client.Close();
+                Console.WriteLine("Client disconnected");
+            
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e}");
+            }
+            finally
+            {
+                server.Stop();
+                Console.WriteLine("Server shut down");
+            }
+        }
+    }
+}
